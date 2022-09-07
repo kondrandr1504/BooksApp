e0d341b5524fb9ade65cd4c9dff7eeccc1f79e26
kondrandr <andreymkt@yandex.ru>
2022-09-07 09:23:35 +0300

    Com1 Инициализация Backend части
    Создание и инициализация backend  части проекта. Добавление всех пакетов и зависимостей. Создание Docker-compose

---
+++ backend/books_service/Startup.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Books.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+
+namespace Books.Service
+{
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
+    {
+      _configuration = configuration;
+    }
+
+    public IConfiguration _configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddDbContext<BooksDatabaseContext>(options =>
+            {
+              options
+                  .UseNpgsql(_configuration.GetConnectionString("DefaultConnection"));
+            });
+
+      // AutoMapper
+      var mapperConfig = new MapperConfiguration(
+          mc =>
+          {
+            mc.AddProfile(new BooksMappingProfile());
+          }
+      );
+      services.AddSingleton(mapperConfig.CreateMapper());
+
+      services.AddScoped<IBooksService, BooksService>();
+
+      services.AddControllers();
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "books_service", Version = "v1" });
+      });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+        app.UseSwagger();
+        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.yaml", "books_service v1"));
+      }
+
+      app.UseHttpsRedirection();
+
+      app.UseRouting();
+
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
+}
+
